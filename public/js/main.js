90bd469ba49397618c1f342fbca5aa1e01596d1b
Khoi Nguyen <khoi.ntq@gmail.com>
2018-11-06 22:30:03 -0500

    Initial Commit

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+const articles = document.getElementById('articles');
+
+if (articles) {
+  articles.addEventListener('click', e => {
+    if (e.target.className === 'btn btn-danger delete-article') {
+      if (confirm('Are you sure to remove the article?')) {
+        const id = e.target.getAttribute('data-id');
+        // fetch
+        fetch(`/article/delete/${id}`, {
+          method: 'DELETE'
+        }).then(res => window.location.reload());
+      }
+    }
+  });
+}
